c677eac355d55f30078ee419307491e3c37b58be
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-30 09:43:35 +0800

    chore: 增加零宽字符串的判断

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,32 @@ export async function activate(context: ExtensionContext) {
   const disposes: Disposable[] = []
   const lan = getLocale()
   const isZh = lan.includes('zh')
-
+  const zero_character_reg = /\p{Cf}/gu
   const fixedNameFunc = (files: any, isEdit = true) => {
     files.forEach((file: any) => {
       const newUri = isEdit ? file.newUri : file
       const ext = basename(newUri.fsPath)
+      const fixedName = ext.replace(/\s/g, '').replace(zero_character_reg, '')
+
       if (/\s/.test(ext)) {
         message.error({
           message: `${ext} ${isZh ? '命名中存在空格,是否自动修复删除空格？' : 'If there is a space in the name, will the space be automatically repaired and deleted?'}`,
           buttons: isZh ? '修复' : 'Repair',
         }).then(async (v) => {
           if (v) {
-            const fixedName = ext.replace(/\s/g, '')
+            workspace.fs.rename(newUri, Uri.file(newUri.fsPath.replace(ext, fixedName)))
+              .then(() => {
+                message.info(`${isZh ? '已将文件名' : 'The file name has been'}：[${ext}] -> [${fixedName}]`)
+              })
+          }
+        })
+      }
+      else if (zero_character_reg.test(ext)) {
+        message.error({
+          message: `${ext} ${isZh ? '命名中存在零宽字符,是否自动修复删除空格？' : 'There are zero-width characters in the name, does it automatically repair and delete spaces?'}`,
+          buttons: isZh ? '修复' : 'Repair',
+        }).then(async (v) => {
+          if (v) {
             workspace.fs.rename(newUri, Uri.file(newUri.fsPath.replace(ext, fixedName)))
               .then(() => {
                 message.info(`${isZh ? '已将文件名' : 'The file name has been'}：[${ext}] -> [${fixedName}]`)
